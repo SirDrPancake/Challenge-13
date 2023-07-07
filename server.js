d0e6b1d89af86473682cf1cb79856eb63e99eb46
joel Quezada <halo.joel.quezada@gmail.com>
2023-07-06 19:00:38 -0700

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
-const Sequelize = require('sequelize');
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
-
-module.exports = sequelize;
